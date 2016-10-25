a7a7811abf7bd5a1b6f95f6959cfd448810dc678
Ben Loveridge <benl@gradecam.com>
2016-10-25 08:57:24 -0600

    add build artifacts

--- dist/ng-i18next.js
+++ dist/ng-i18next.js
@@ -1,5 +1,5 @@
 /*!
- * ng-i18next - Version 1.0.1 - 2016-09-26
+ * ng-i18next - Version 1.0.2 - 2016-10-25
  * Copyright (c) 2016 Andre Meyering
  *
  * AngularJS provider, filter and directive for i18next (i18next by Jan Mühlemann)
@@ -116,6 +116,7 @@ var I18nDirectiveController = (function () {
     };
     
     I18nDirectiveController.prototype.parse = function (key, noWatch) {
+        var _this = this;
         var parsedKey = this.parseKey(key);
         // If there are watched values, unregister them
         if (this.argsUnregister) {
@@ -126,8 +127,8 @@ var I18nDirectiveController = (function () {
         }
         if (!noWatch) {
             this.argsUnregister = this.$scope.$watch(function () {
-                return parsedKey.i18nOptions;
-            }, this.render, true);
+                return parsedKey.i18nOptions(_this.$scope);
+            }, function () { return _this.render(parsedKey, noWatch); }, true);
         }
         this.render(parsedKey, noWatch);
     };
